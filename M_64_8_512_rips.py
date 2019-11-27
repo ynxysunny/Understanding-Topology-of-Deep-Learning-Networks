3df94fc8ddd87b2415a99f41e4151b1a3ff94281
ClaudiaHeYun <53270051+ClaudiaHeYun@users.noreply.github.com>
2019-11-27 10:32:48 -0500

    persistent homology
    added a preprocess function and computed persistent homology of the weights

---
+++ M_64_8_512_rips.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 21:24:38 2019
+
+@author: Yun
+
+Calculate persistent homology of the weights collected from M(64,8,512)
+"""
+
+import numpy as np
+from ripser import ripser
+from persim import plot_diagrams
+from sklearn import datasets
+from preprocess import process
+
+
+# read in weights
+filtered_weights = process('weights_64_8_512.csv')
+#data = datasets.make_circles(n_samples=100)[0] + 5 * datasets.make_circles(n_samples=100)[0]
+diagrams = ripser(filtered_weights)['dgms']
+plot_diagrams(diagrams, show=True)
+
